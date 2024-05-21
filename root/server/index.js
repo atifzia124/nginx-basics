1789a6e5b4e0deec95ed575d2ad026d43f0a5150
atifzia124 <atifziaabdulnabi124@gmail.com>
2024-05-21 15:03:36 +0530

    nginx basics

---
+++ root/server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("i am endpoint");
+});
+
+app.listen(7777, () => {
+  console.log("listening to server");
+});
